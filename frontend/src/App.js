ce469c926c8a1a21859450433bac6b6841986f5f
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2023-04-05 22:00:43 +0700

    ASM-2-JS

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href="/">Amazon</a>
+      </header>
+      <main>
+        Products
+      </main>
+    </div>
+  );
+}
+
+export default App;
