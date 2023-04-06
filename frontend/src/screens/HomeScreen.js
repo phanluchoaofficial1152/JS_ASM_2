2a0dad8460b063f45571b36c701d593212a6d85f
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2023-04-06 21:23:14 +0700

    up

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
-import { Link } from "react-router-dom";
 // import data from "../data";
 import axios from 'axios';
 // eslint-disable-next-line
 import { useEffect, useReducer, useState } from "react";
 import logger from 'use-reducer-logger';
 import React  from 'react';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Product from "../components/Product";
 
 const reducer = (state , action) => {
   switch (action.type) {
@@ -62,20 +64,16 @@ function HomeScreen () {
           )
           :
           (
-            products.map((product) => (
-              <div className="product" key={product.slug}>
-                <Link to={`/product/${product.slug}`}>
-                  <img src={product.image} alt={product.name} />
-                </Link>
-                <div className="product-info">
-                  <Link to={`/product/${product.slug}`}>
-                    <p>{product.name}</p>
-                  </Link>
-                    <p><strong>${product.price}</strong></p>
-                    <button className="btn-cart">Add to cart</button>
-                </div>
-              </div>
-              ))
+            <Row>
+              {
+                products.map((product) =>
+                (
+                  <Col key={product.slug} sm={6} md={4} lg={3} className="mb-3">
+                    <Product product={product}></Product>
+                  </Col>
+                ))
+              }
+              </Row>
           )}
           </div>
     </div>
