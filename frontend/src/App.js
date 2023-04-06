a405ecffb6c614546fb344b95c253540fae30f46
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2023-04-06 16:38:11 +0700

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,36 @@
-import data from "./data";
+import data from "./data"
 
 function App() {
   return (
     <div className="App">
-      <header>
+      <header className="title-header">
         <a href="/">Amazon</a>
       </header>
       <main>
-        <h1>Featured Products</h1>
+        <h1 className="title-featured">Featured Products</h1>
+        <div className="products">
         {
           data.products.map(product => (
-          <div>
-            <img src={product.image} alt={product.name} />
-            <p>
-              {product.name}
-            </p>
-            <p>
-              {product.price}
-            </p>
-          </div>
-          ))};
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>
+                    {product.name}
+                  </p>
+                </a>
+                  <p>
+                    <strong>${product.price}</strong>
+                  </p>
+                  <button>
+                      Add to cart
+                  </button>
+              </div>
+            </div>
+            ))}
+        </div>
       </main>
     </div>
   );
