98828a261b7d82a7ecc910b7b655756a037f94de
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2023-04-07 12:27:35 +0700

    2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import {LinkContainer} from 'react-router-bootstrap';
 import Badge from 'react-bootstrap/esm/Badge';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
   const {state} = useContext(Store);
@@ -45,6 +46,7 @@ function App() {
             <Routes>
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
