7db8fd90547376dae71ebe654807cfdf0b7682b8
phanluchoaofficial1152 <shopquanaothucdoan@gmail.com>
2023-04-08 00:12:57 +0700

    1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,6 +14,7 @@ import Badge from 'react-bootstrap/esm/Badge';
 import { Store } from './Store';
 import CartScreen from './screens/CartScreen';
 import SigninScreen from './screens/SigninScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 
 function App() {
   const {state , dispatch: ctxDispatch} = useContext(Store);
@@ -23,6 +24,7 @@ function App() {
   const signoutHandler = () => {
       ctxDispatch({type: 'USER_SIGNOUT'});
       localStorage.removeItem('userInfo');
+      localStorage.removeItem('shippingAddress');
   };
 
   return (
@@ -81,6 +83,7 @@ function App() {
               <Route path="/product/:slug" element={<ProductScreen />} />
               <Route path="/cart" element={<CartScreen />} />
               <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/shipping" element={<ShippingAddressScreen />} />
               <Route path="/" element={<HomeScreen />} />
             </Routes>
           </Container>
